a44286ee998f24abe6a0cc05b92d60fd62094ae8
vinaysuyal <vinaysuyal2012@gmail.com>
2023-08-31 17:47:21 +0530

    Major features/styles added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import PreText from "./Component/PreTest.component";
+import { useState } from "react";
+import Quiz from "./Component/Quiz.component";
 
 function App() {
+  const [answers, setAnswers] = useState([]);
+  const [questions, setQuestions] = useState([]);
+  const [testState, setTestState] = useState(0);
+  const [email, setEmail] = useState("");
+  const onEmailChange = (newEmail) => {
+    setEmail(newEmail);
+  };
+  const moveNextStep = () => {
+    setTestState((prevTestState) => prevTestState + 1);
+  };
+  const onSetAnswer = (questionNumber, answer) => {
+    setAnswers((prevAnswers) => {
+      const newAnswers = [...prevAnswers];
+      newAnswers[questionNumber] = answer;
+      return newAnswers;
+    });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {testState === 0 && (
+        <PreText
+          email={email}
+          onEmailChange={onEmailChange}
+          onEmailSubmission={moveNextStep}
+        />
+      )}
+      {testState != 0 && (
+        <Quiz
+          onSetAnswer={onSetAnswer}
+          questions={questions}
+          setQuestions={(questions) => {
+            setQuestions(questions);
+            let newAnswers = new Array(questions.length).fill(null);
+            setAnswers(newAnswers);
+          }}
+          answers={answers}
+          onQuizSubmission={moveNextStep}
+          mode={testState === 1 ? "quiz" : "report"}
+        />
+      )}
     </div>
   );
 }
